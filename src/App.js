b6e5f9f778bdb95ab871b5560dce1680e4c784fc
asaldavodi1328-ship-it <asal.davodi1328@gmail.com>
2026-02-18 21:05:55 -0500

    Student NAME Added

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ const App = () => {
   return (
     <div className="App">
       <h1>Student ID: 101492107</h1>
-      <h1>Studen ID: Put Your Student Name</h1>
+      <h1>Student ID: Asal Davoudi</h1>
       <h1>Student Information System</h1>
       <h5>Powered By: moxDroid Labs Inc. || DevOps</h5>
       <StudentForm addStudent={addStudent} />
